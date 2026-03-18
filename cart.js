8a8e86b61d5e4fa4faa166b67499762d4eb3470a
dmitry_p <dmitry_p@example.com>
2026-03-19 00:02:48 +0300

    feat(cart): реализована функция обновления количества

--- cart.js
+++ cart.js
@@ -30,7 +30,41 @@ function renderCart() {
     emptyMsg.style.display = 'none';
     table.style.display    = 'table';
     totalDiv.style.display = 'block';
-    // Рендер строк — добавишь в ПОРЦИИ 4
+    tbody.innerHTML = cart.map((item, index) => {
+        const product = products.find(p => p.id === item.id);
+        const sum = product.price * item.quantity;
+        return `<tr>
+            <td>${index + 1}</td>
+            <td>${product.image} ${product.name}</td>
+            <td>${product.price.toLocaleString('ru-RU')} ₽</td>
+            <td>
+                <button onclick="updateQuantity(${product.id},-1)">−</button>
+                ${item.quantity}
+                <button onclick="updateQuantity(${product.id}, 1)">+</button>
+            </td>
+            <td>${sum.toLocaleString('ru-RU')} ₽</td>
+            <td>
+                <button onclick="removeFromCart(${product.id})"
+                        class="btn-remove">✕</button>
+            </td>
+        </tr>`;
+    }).join('');
+    document.getElementById('totalPrice').textContent =
+    calculateTotal().toLocaleString('ru-RU');
+
 }
-  
+
+function updateQuantity(productId, delta) {
+    let cart = getCart();
+    const item = cart.find(item => item.id === productId);
+    if (item) {
+        item.quantity += delta;
+        if (item.quantity <= 0)
+            cart = cart.filter(i => i.id !== productId);
+        saveCart(cart);
+        renderCart();
+        updateBadges();
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => { renderCart(); });
